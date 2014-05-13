3eec3cca9281b6f3088cc467790bb0158b44f449
Michael Ferguson <fergs@unboundedrobotics.com>
2014-05-13 01:21:59 -0700

    fix python install

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     scripts=['scripts/robot_calibration'],
-    packages=['ubr_calibration'],
     )
 
 setup(**d)
